91c39bacccd6a8eeca8d974f0a142ca1060be0c1
huappe <155170805+huappe@users.noreply.github.com>
2020-09-23 22:35:39 +0000

    Add node_modules

--- app/src/androidTest/java/com/toure/objectdetection/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/toure/objectdetection/ExampleInstrumentedTest.java
@@ -9,3 +9,8 @@ import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Tes
