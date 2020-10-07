25e4517037cb5ca651ffb4095b2ba7065b47ce49
huappe <155170805+huappe@users.noreply.github.com>
2020-10-07 18:17:51 +0000

    Removed dependency

--- app/src/androidTest/java/com/toure/objectdetection/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/toure/objectdetection/ExampleInstrumentedTest.java
@@ -13,4 +13,9 @@ import static org.junit.Assert.assertEquals;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Tes
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext(
