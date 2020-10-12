02ccd7cbe9cb8e9517cabab20f014f62ce5901be
huappe <155170805+huappe@users.noreply.github.com>
2020-10-12 10:09:15 +0000

    Update VM.md

--- app/src/androidTest/java/com/toure/objectdetection/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/toure/objectdetection/ExampleInstrumentedTest.java
@@ -22,4 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.tour
+        assertEquals("com.toure.objectdetection", appContext.getPackageName());
+    }
+}
