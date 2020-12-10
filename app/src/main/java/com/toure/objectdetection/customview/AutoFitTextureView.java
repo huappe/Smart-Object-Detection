ebb758965af55e5947321938910dbf54b274d3c9
huappe <155170805+huappe@users.noreply.github.com>
2020-12-10 15:45:51 +0000

    top

--- app/src/main/java/com/toure/objectdetection/customview/AutoFitTextureView.java
+++ app/src/main/java/com/toure/objectdetection/customview/AutoFitTextureView.java
@@ -46,4 +46,8 @@ public class AutoFitTextureView extends TextureView {
    * @param height Relative vertical size
    */
   public void setAspectRatio(final int width, final int height) {
-    if (width < 0 || heig
+    if (width < 0 || height < 0) {
+      throw new IllegalArgumentException("Size cannot be negative.");
+    }
+    ratioWidth = width;
+    ratioHeight = height
