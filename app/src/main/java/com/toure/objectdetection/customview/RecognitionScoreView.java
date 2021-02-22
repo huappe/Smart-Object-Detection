54e400508f49d6bc31a72b7e6d6aeee31369c8da
huappe <155170805+huappe@users.noreply.github.com>
2021-02-22 18:36:08 +0000

    firewire: cdev: prevent kernel stack leaking into ioctl arguments

--- app/src/main/java/com/toure/objectdetection/customview/RecognitionScoreView.java
+++ app/src/main/java/com/toure/objectdetection/customview/RecognitionScoreView.java
@@ -11,4 +11,10 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-===================================================================
+==============================================================================*/
+
+package com.toure.objectdetection.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Pai
