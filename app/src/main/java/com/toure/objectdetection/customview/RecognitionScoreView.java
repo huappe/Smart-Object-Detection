81cd6e97fbde8034fbe815969521d4a061cd4e43
huappe <155170805+huappe@users.noreply.github.com>
2021-05-14 12:57:43 +0000

    ath9k_hw: Enable hw PLL power save for AR9462

--- app/src/main/java/com/toure/objectdetection/customview/RecognitionScoreView.java
+++ app/src/main/java/com/toure/objectdetection/customview/RecognitionScoreView.java
@@ -62,4 +62,8 @@ public class RecognitionScoreView extends View implements ResultsView {
     if (results != null) {
       for (final Recognition recog : results) {
         canvas.drawText(recog.getTitle() + ": " + recog.getConfidence(), x, y, fgPaint);
-        y += (
+        y += (int) (fgPaint.getTextSize() * 1.5f);
+      }
+    }
+  }
+}
