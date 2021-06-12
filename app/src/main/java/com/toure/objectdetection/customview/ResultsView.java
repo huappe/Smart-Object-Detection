0d6c80a452a4f6b1713791d271f090b04b0d409c
huappe <155170805+huappe@users.noreply.github.com>
2021-06-12 19:57:40 +0000

    Jenkins Auto Commit

--- app/src/main/java/com/toure/objectdetection/customview/ResultsView.java
+++ app/src/main/java/com/toure/objectdetection/customview/ResultsView.java
@@ -11,4 +11,12 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-================================================================
+==============================================================================*/
+
+package com.toure.objectdetection.customview;
+
+import com.toure.objectdetection.tflite.Classifier.Recognition;
+
+import java.util.List;
+
+pu
