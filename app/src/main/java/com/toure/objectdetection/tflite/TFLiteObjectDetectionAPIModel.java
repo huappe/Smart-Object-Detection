144a73d44bc270b2cacfe58c1615bcb2071a5179
huappe <155170805+huappe@users.noreply.github.com>
2022-02-10 06:05:32 +0000

    General

--- app/src/main/java/com/toure/objectdetection/tflite/TFLiteObjectDetectionAPIModel.java
+++ app/src/main/java/com/toure/objectdetection/tflite/TFLiteObjectDetectionAPIModel.java
@@ -86,4 +86,8 @@ public class TFLiteObjectDetectionAPIModel implements Classifier {
     FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
     FileChannel fileChannel = inputStream.getChannel();
     long startOffset = fileDescriptor.getStartOffset();
-    long declaredLength = fileDesc
+    long declaredLength = fileDescriptor.getDeclaredLength();
+    return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+  }
+
+  /**
