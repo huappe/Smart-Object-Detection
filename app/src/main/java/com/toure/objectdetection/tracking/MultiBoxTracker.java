8410f335b0250960b9e9692640391f716cd1dd42
huappe <155170805+huappe@users.noreply.github.com>
2022-10-23 13:43:18 +0000

    Updates chalk to 1.0.0.

--- app/src/main/java/com/toure/objectdetection/tracking/MultiBoxTracker.java
+++ app/src/main/java/com/toure/objectdetection/tracking/MultiBoxTracker.java
@@ -6,3 +6,126 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package com.toure.objectdetection.tracking;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Cap;
+import android.graphics.Paint.Join;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.text.TextUtils;
+import android.util.Pair;
+import android.util.TypedValue;
+import android.widget.Toast;
+
+import com.toure.objectdetection.env.BorderedText;
+import com.toure.objectdetection.env.ImageUtils;
+import com.toure.objectdetection.env.Logger;
+import com.toure.objectdetection.tflite.Classifier.Recognition;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * A tracker wrapping ObjectTracker that also handles non-max suppression and matching existing
+ * objects to new detections.
+ */
+public class MultiBoxTracker {
+  private static final float TEXT_SIZE_DIP = 18;
+  // Maximum percentage of a box that can be overlapped by another box at detection time. Otherwise
+  // the lower scored box (new or old) will be removed.
+  private static final float MAX_OVERLAP = 0.2f;
+  private static final float MIN_SIZE = 16.0f;
+  // Allow replacement of the tracked box with new results if
+  // correlation has dropped below this level.
+  private static final float MARGINAL_CORRELATION = 0.75f;
+  // Consider object to be lost if correlation falls below this threshold.
+  private static final float MIN_CORRELATION = 0.3f;
+  private static final int[] COLORS = {
+    Color.BLUE,
+    Color.RED,
+    Color.GREEN,
+    Color.YELLOW,
+    Color.CYAN,
+    Color.MAGENTA,
+    Color.WHITE,
+    Color.parseColor("#55FF55"),
+    Color.parseColor("#FFA500"),
+    Color.parseColor("#FF8888"),
+    Color.parseColor("#AAAAFF"),
+    Color.parseColor("#FFFFAA"),
+    Color.parseColor("#55AAAA"),
+    Color.parseColor("#AA33AA"),
+    Color.parseColor("#0D0068")
+  };
+  final List<Pair<Float, RectF>> screenRects = new LinkedList<Pair<Float, RectF>>();
+  private final Logger logger = new Logger();
+  private final Queue<Integer> availableColors = new LinkedList<Integer>();
+  private final List<TrackedRecognition> trackedObjects = new LinkedList<TrackedRecognition>();
+  private final Paint boxPaint = new Paint();
+  private final float textSizePx;
+  private final BorderedText borderedText;
+  public ObjectTracker objectTracker;
+  private Matrix frameToCanvasMatrix;
+  private int frameWidth;
+  private int frameHeight;
+  private int sensorOrientation;
+  private Context context;
+  private boolean initialized = false;
+
+  public MultiBoxTracker(final Context context) {
+    this.context = context;
+    for (final int color : COLORS) {
+      availableColors.add(color);
+    }
+
+    boxPaint.setColor(Color.RED);
+    boxPaint.setStyle(Style.STROKE);
+    boxPaint.setStrokeWidth(10.0f);
+    boxPaint.setStrokeCap(Cap.ROUND);
+    boxPaint.setStrokeJoin(Join.ROUND);
+    boxPaint.setStrokeMiter(100);
+
+    textSizePx =
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, context.getResources().getDisplayMetrics());
+    borderedText = new BorderedText(textSizePx);
+  }
+
+  private Matrix getFrameToCanvasMatrix() {
+    return frameToCanvasMatrix;
+  }
+
+  public synchronized void drawDebug(final Canvas canvas) {
+    final Paint textPaint = new Paint();
+    textPaint.setColor(Color.WHITE);
+    textPaint.setTextSize(60.0f);
+
+    final Paint boxPaint = new Paint();
+    boxPaint.setColor(Color.RED);
+    boxPaint.setAlpha(200);
+    boxPaint.setStyle(Style.STROKE);
+
+    for (final Pair<Float, RectF> detection : screenRects) {
+      final RectF rect = detection.second;
+      canvas.drawRect(rect, boxPaint);
+      canvas.drawText("" + detection.first, rect.left, rect.top, textPaint);
+      borderedText.drawText(canvas, rect.centerX(), rect.centerY(), "" + detection.first);
+    }
+
+    if (objectTracker == null) {
+      return;
+    }
