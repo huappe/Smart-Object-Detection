c30b63e186902105e747d61fe564c2ac0f070e44
huappe <155170805+huappe@users.noreply.github.com>
2023-08-26 05:34:36 +0000

    art

---
+++ app/src/test/java/com/toure/objectdetection/ExampleUnitTest.java
@@ -0,0 +1,10 @@
+
+package com.toure.objectdetection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
